9926319c53a4a42e2d58baa4e1794502cf40de9a
Clinton Raj <clintonraj007@outlook.com>
2023-11-21 13:57:08 +0530

    change cucumber.java.en

--- src/main/java/com/step/StepDefinition.java
+++ src/main/java/com/step/StepDefinition.java
@@ -2,6 +2,7 @@ package com.step;
 
 import java.util.concurrent.TimeUnit;
 
+
 import com.pojo.BaseClass;
 import com.pojo.Booking;
 import com.pojo.Cancellation;
@@ -9,9 +10,13 @@ import com.pojo.Login;
 import com.pojo.Search;
 import com.pojo.Select;
 
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
+//import cucumber.api.java.en.Given;
+//import cucumber.api.java.en.Then;
+//import cucumber.api.java.en.When;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 public class StepDefinition extends BaseClass {
 	
