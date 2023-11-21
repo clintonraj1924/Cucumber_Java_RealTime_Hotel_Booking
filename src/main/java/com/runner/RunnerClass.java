3ea4dbe6925ed30172b46d5996666dafb63a570c
Clinton Raj <clintonraj007@outlook.com>
2023-11-21 15:06:46 +0530

    added runner class

--- src/main/java/com/runner/RunnerClass.java
+++ src/main/java/com/runner/RunnerClass.java
@@ -1,5 +1,19 @@
 package com.runner;
 
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features=".//Feature/Cancellation.feature",
+		glue="com.step",
+		dryRun=true,
+		plugin= {"pretty", 
+				"html:test-output"}
+		)
+
+
 public class RunnerClass {
 
 }
