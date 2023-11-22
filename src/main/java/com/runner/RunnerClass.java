cdba19a39b327209ae5005e1049d2de6ae6987ac
Clinton Raj <clintonraj007@outlook.com>
2023-11-22 12:14:20 +0530

    pom modification

--- src/main/java/com/runner/RunnerClass.java
+++ src/main/java/com/runner/RunnerClass.java
@@ -1,11 +1,10 @@
 package com.runner;
 
 import org.junit.runner.RunWith;
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(
+@CucumberOptions
+(
 		features=".//Feature/Cancellation.feature",
 		glue="com.step",
 		dryRun=true,
